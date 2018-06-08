e25afcb649d308f6505a14cb8328400ee8b20a86
Sean Gallagher <seanpg94@gmail.com>
2018-06-08 09:33:03 -0500

    Update MainActivity.java

--- Sample/src/main/java/com/phunware/engagement/sample/activities/MainActivity.java
+++ Sample/src/main/java/com/phunware/engagement/sample/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
